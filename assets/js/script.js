fd97335f7f12967984dbd6b95b5e88b92c4a141a
Sylvie et Lhétie <sylvie@MacBook-Pro-de-Sylvie.local>
2020-05-04 12:31:22 +0200

    Corrigé Exe5

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+function divideBy()
+{
+  var firstNumber = document.getElementById('firstNumber').value;
+  var secondNumber = document.getElementById('secondNumber').value;
+  var result = firstNumber % secondNumber;
+alert(result);
+}
